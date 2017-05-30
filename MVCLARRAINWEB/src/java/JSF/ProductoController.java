d94040ba9745b441508fa7d60703e0b2e288ae74
pablo88 <pablo_silva88@hotmail.com>
2017-05-30 14:18:34 -0400

    cambios al producto controller

--- MVCLARRAINWEB/src/java/JSF/ProductoController.java
+++ MVCLARRAINWEB/src/java/JSF/ProductoController.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -34,18 +35,8 @@ public class ProductoController implements Serializable {
     private SessionBeans.TipoProductoFacade ejbFacadeTP;
     private PaginationHelper pagination;
     private int selectedItemIndex;
-    private List<Integer> ids;
-    
-    public List<Integer> getIds()
-    {
-        return ids;
-    }
-    
-    public void setIds(int numero)
-    {
-        ids.add(numero);
-    }
-    
+    private List<Producto> productos;
+
     public ProductoController() {
     }
 
@@ -68,234 +59,14 @@ public class ProductoController implements Serializable {
     private ProductoFacade getFacade() {
         return ejbFacade;
     }
-
-    public PaginationHelper getPagination() {
-        if (pagination == null) {
-            pagination = new PaginationHelper(10) {
-
-                @Override
-                public int getItemsCount() {
-                    return getFacade().count();
-                }
-
-                @Override
-                public DataModel createPageDataModel() {
-                    return new ListDataModel(getFacade().findRange(new int[]{getPageFirstItem(), getPageFirstItem() + getPageSize()}));
-                }
-            };
-        }
-        return pagination;
+    
+    @PostConstruct
+    private void init() {
+        productos=ejbFacade.findAll();
     }
-
-    public String prepareList() {
-        recreateModel();
-        return "List";
+    
+    public List<Producto> getProductos()
+    {
+        return productos;
     }
-
-    public String prepareView() {
-        current = (Producto) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
-        return "View";
-    }
-
-    public String prepareCreate() {
-        current = new Producto();
-        selectedItemIndex = -1;
-        return "Create";
-    }
-
-    public String create() {
-        try {
-            getFacade().create(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ProductoCreated"));
-            return prepareCreate();
-        } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
-        }
-    }
-
-    public String prepareEdit() {
-        current = (Producto) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
-        return "Edit";
-    }
-
-    public String update() {
-        try {
-            getFacade().edit(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ProductoUpdated"));
-            return "View";
-        } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
-        }
-    }
-
-    public String destroy() {
-        current = (Producto) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
-        performDestroy();
-        recreatePagination();
-        recreateModel();
-        return "List";
-    }
-
-    public String destroyAndView() {
-        performDestroy();
-        recreateModel();
-        updateCurrentItem();
-        if (selectedItemIndex >= 0) {
-            return "View";
-        } else {
-            // all items were removed - go back to list
-            recreateModel();
-            return "List";
-        }
-    }
-
-    private void performDestroy() {
-        try {
-            getFacade().remove(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("ProductoDeleted"));
-        } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-        }
-    }
-
-    private void updateCurrentItem() {
-        int count = getFacade().count();
-        if (selectedItemIndex >= count) {
-            // selected index cannot be bigger than number of items:
-            selectedItemIndex = count - 1;
-            // go to previous page if last page disappeared:
-            if (pagination.getPageFirstItem() >= count) {
-                pagination.previousPage();
-            }
-        }
-        if (selectedItemIndex >= 0) {
-            current = getFacade().findRange(new int[]{selectedItemIndex, selectedItemIndex + 1}).get(0);
-        }
-    }
-
-    public DataModel getItems() {
-        if (items == null) {
-            items = getPagination().createPageDataModel();
-        }
-        return items;
-    }
-
-    private void recreateModel() {
-        items = null;
-    }
-
-    private void recreatePagination() {
-        pagination = null;
-    }
-
-    public String next() {
-        getPagination().nextPage();
-        recreateModel();
-        return "List";
-    }
-
-    public String previous() {
-        getPagination().previousPage();
-        recreateModel();
-        return "List";
-    }
-
-    public SelectItem[] getItemsAvailableSelectMany() {
-        return JsfUtil.getSelectItems(ejbFacade.findAll(), false);
-    }
-
-    public SelectItem[] getItemsAvailableSelectOne() {
-        return JsfUtil.getSelectItems(ejbFacade.findAll(), true);
-    }
-
-    public Producto getProducto(java.math.BigDecimal id) {
-        return ejbFacade.find(id);
-    }
-
-    @FacesConverter(forClass = Producto.class)
-    public static class ProductoControllerConverter implements Converter {
-
-        @Override
-        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
-                return null;
-            }
-            ProductoController controller = (ProductoController) facesContext.getApplication().getELResolver().
-                    getValue(facesContext.getELContext(), null, "productoController");
-            return controller.getProducto(getKey(value));
-        }
-
-        java.math.BigDecimal getKey(String value) {
-            java.math.BigDecimal key;
-            key = new java.math.BigDecimal(value);
-            return key;
-        }
-
-        String getStringKey(java.math.BigDecimal value) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(value);
-            return sb.toString();
-        }
-
-        @Override
-        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-            if (object == null) {
-                return null;
-            }
-            if (object instanceof Producto) {
-                Producto o = (Producto) object;
-                return getStringKey(o.getIdProducto());
-            } else {
-                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + Producto.class.getName());
-            }
-        }
-
-    }
-
-    public List obtenerProductos() {
-        try {
-
-            List<Producto> listaProducto = ejbFacade.findAll();
-            List<String[]> listaFotos = new ArrayList<>();
-            int i = 0;
-            for (Producto foto : listaProducto) {
-                String[] indata = new String[6];
-                indata[0] = null;
-                indata[1] = foto.getNombreProducto();
-                indata[2] = foto.getImagenProducto();
-                indata[3] = "$ " + foto.getPrecioProducto();
-                indata[4] = "Stock: " + foto.getStockProducto();
-                indata[5] = "" + foto.getIdProducto();
-                listaFotos.add(indata);
-            }
-            return listaFotos;
-        } catch (Exception ex) {
-            return null;
-        }
-
-    }
-
-    public List<Producto> obtenerProductosTipo() {
-        try {
-            List<Producto> productos = new ArrayList<Producto>();
-            for (int numero : ids) {
-                for (Producto prod : getFacade().findAll()) {
-                    if(prod.getIdProducto().intValueExact()==numero)
-                    {
-                        productos.add(prod);
-                    }
-                }
-            }
-            return productos;
-        } catch (Exception ex) {
-
-        }
-        return null;
-    }
-
 }
