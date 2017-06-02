4e5bee467813b5f312e164350be622e8090efc3e
KapriDoo <cipriano.rivera@live.cl>
2017-06-02 00:28:04 -0400

    pruebas unitarias

---
+++ MVCLARRAINWEB/test/JSF/ProductoControllerTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JSF;
+
+import Models.Producto;
+import Models.TipoProducto;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Home
+ */
+public class ProductoControllerTest {
+    
+    public ProductoControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getSelected method, of class ProductoController.
+     */
+    @Test
+    public void testGetSelected() {
+        System.out.println("getSelected");
+        ProductoController instance = new ProductoController();
+        Producto expResult = null;
+        Producto result = instance.getSelected();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSelected1 method, of class ProductoController.
+     */
+    @Test
+    public void testGetSelected1() {
+        System.out.println("getSelected1");
+        ProductoController instance = new ProductoController();
+        TipoProducto expResult = null;
+        TipoProducto result = instance.getSelected1();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getProductos method, of class ProductoController.
+     */
+    @Test
+    public void testGetProductos() {
+        System.out.println("getProductos");
+        ProductoController instance = new ProductoController();
+        List<Producto> expResult = null;
+        List<Producto> result = instance.getProductos();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
